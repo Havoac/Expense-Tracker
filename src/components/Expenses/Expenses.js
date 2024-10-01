0b0265b7237a3660517bced667f74d3d3f1ae87e
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-10-01 08:46:14 +0530

    Filter by year structure added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import FilterItem from "./FilterItem";
 
 function Expenses(props) {
+  const [filteredYear, SetFilteredYear] = useState("2020");
+
+  const ChangeFilterHandler = (selectedYear) => {
+    SetFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <FilterItem
+        selectedYear={filteredYear}
+        OnChangeFilter={ChangeFilterHandler}
+      />
       <ExpenseItem expenseData={props.expenseData[0]} />
       <ExpenseItem expenseData={props.expenseData[1]} />
       <ExpenseItem expenseData={props.expenseData[2]} />
---
+++ src/components/Expenses/FilterItem.js
@@ -0,0 +1,23 @@
+import "./FilterItem.css";
+
+const FilterItem = (props) => {
+  const ChangeYearHandler = (event) => {
+    props.OnChangeFilter(event.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selectedYear} onChange={ChangeYearHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default FilterItem;
